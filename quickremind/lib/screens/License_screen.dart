9efa20235e919bdad21771c8c2b83fd8f403741c
OMEGA <lightsound2321@gmail.com>
2025-03-03 17:55:07 +0900

    refactor: rename to appropriate file name

--- quickremind/lib/screens/License_screen.dart
+++ quickremind/lib/screens/License_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LicenseScreen extends StatelessWidget {
-  const LicenseScreen({Key? key}) : super(key: key);
+  const LicenseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
